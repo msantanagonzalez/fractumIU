7e6c6d35f8d980a9b12efbcc5018d29bc30a3984
msgonzalez <msgonzalez@esei.uvigo.es>
2015-01-12 19:37:37 +0100

    Gestion Maquinas

---
+++ www/Controller/maquinasController.php
@@ -0,0 +1,156 @@
+<?php
+	require_once("../Model/maquina.php");
+	include_once 'bdController.php';
+
+	if(isset($_GET['accion'])){	$accion = $_GET['accion']; }
+	if(isset($_POST['accion'])){ $accion = $_POST['accion']; }
+
+	
+	switch ($accion) {
+		case 'Alta':
+			alta();
+			break;
+		case 'Consulta':
+			consulta();
+			break;
+		case 'Modificar':
+			modificar();
+			break;
+		case 'Guardar':
+			modificado();
+			break;
+		case 'Listar':
+			lista();
+			break;
+		case 'Buscar':
+			buscar();
+			break;
+		case 'Eliminar':
+			eliminar();
+			break;
+	}
+
+	function alta(){
+		session_start();
+
+		$maquina = new Maquina($_POST["idMaq"], $_POST["nSerie"], $_POST["descripMaq"],$_POST["nomMaq"], $_POST["costeMaq"]);
+		$maquina->alta();
+		$tempMaquina = $_POST["idMaq"];
+		header("location: maquinasController.php?accion=Consulta&idMaq=$tempMaquina");
+	}
+
+	function consulta(){
+		session_start();
+
+		$maquina = new Maquina();
+
+		$idMaquina = $_REQUEST['idMaq'];
+		$consultaMaquina = $maquina->consultaMaquina($idMaquina);
+		$consulta = array();		
+		while($row = mysql_fetch_array($consultaMaquina)){
+			array_push($consulta, $row);
+		}
+
+		$_SESSION["consultaMaquina"] = $consulta;
+	
+		switch ($_SESSION['tipo']) {
+			case 'J':
+				session_write_close();
+				header("location: ../View/maquinas/consultarJefe.php");
+				break;
+			case 'I':
+				header("location: ../View/maquinas/consultarInterno.php");
+				session_write_close();
+				break;
+			case 'E':
+				header("location: ../View/maquinas/consultarExterno.php");
+				session_write_close();
+			default:				
+				break;
+		}
+	}
+	function modificar(){
+		session_start();
+		$maquina = new Maquina();
+
+	
+		$idMaquina = $_REQUEST['idMaq'];
+		$consultaMaquina = $maquina->consultaMaquina($idMaquina);
+		
+		$consulta = array();		
+		while($row = mysql_fetch_array($consultaMaquina)){
+			array_push($consulta, $row);
+		}
+
+		$_SESSION["consultaMaquina"] = $consulta;
+
+
+		if($_SESSION['tipo'] == 'J') {
+				session_write_close();
+				header("location: ../View/maquinas/modificarJefe.php");
+				
+		}
+	}
+	function modificado(){
+		session_start();
+		
+		$idMaq = $_POST['idMaq'];
+		$nSerie = $_POST['nSerie'];
+		$descripMaq = $_POST['descripMaq'];
+		$nomMaq = $_POST['nomMaq'];		
+		$costeMaq = $_POST['costeMaq'];
+			
+		$maquina = new Maquina($idMaq, $nSerie, $descripMaq, $nomMaq, $costeMaq);
+		$maquina->modificacion($idMaq);
+		
+		header("location: ../Controller/maquinasController.php?accion=Consulta&idMaq=$idMaq");
+	}
+	
+	function lista(){
+		session_start();
+
+		$maquina = new Maquina();
+		$listaMaquinas = $maquina->lista();
+		$lista = array();
+		
+		while($row = mysql_fetch_array($listaMaquinas)){
+			array_push($lista, $row);
+		}
+		$_SESSION["listaMaquina"] = $lista;
+
+		switch ($_SESSION['tipo']) {
+			case 'J':
+				session_write_close();
+				header("location: ../View/maquinas/listarJefe.php");
+				break;
+			case 'I':
+				header("location: ../View/maquinas/listarInterno.php");
+				session_write_close();
+				break;
+			case 'E':
+				
+				header("location: ../View/maquinas/listarExterno.php");
+				session_write_close();
+			default:				
+				break;
+		}
+	}
+	///comprobar
+					
+	function eliminar(){
+	session_start();
+		$idMaq    = $_GET['idMaq'];
+		$maquina = new Maquina ($idMaq,"","","","");
+		$resultado  = $maquina->consultaMaquina($idMaq);
+		
+		if ($resultado){
+			$maquina->borrar();
+		}
+		header("location: maquinasController.php?accion=Listar");
+		
+			
+		
+		
+	
+	}
+	
---
+++ www/Model/maquina.php
@@ -0,0 +1,75 @@
+<?php 
+/**
+* Clase maquina
+*/
+
+
+class Maquina {
+
+	private $idMaquina;
+	private $numeroSerie;
+	private $descripcionMaquina;
+	private $nombreMaquina;
+	private $costeMaquina;
+	
+	
+/**
+	*	Constructor de maquina.
+	*/
+
+	function __construct($idMaquina = NULL, $numeroSerie = NULL, $descripcionMaquina = NULL, $nombreMaquina = NULL, $costeMaquina = NULL){
+		$this->idMaquina     	   = $idMaquina;
+		$this->numeroSerie    	   = $numeroSerie;
+		$this->descripcionMaquina  = $descripcionMaquina;
+		$this->nombreMaquina       = $nombreMaquina;
+		$this->costeMaquina  	   = $costeMaquina;
+	
+	}
+
+	/**
+	 * Inicio de las funciones SQL
+	 */
+	
+	public function alta(){
+	
+			mysql_query("INSERT INTO MAQUINA(idMaq, nSerie, descripMaq, nomMaq, costeMaq) 
+						VALUES ('$this->idMaquina','$this->numeroSerie','$this->descripcionMaquina','$this->nombreMaquina',
+						'$this->costeMaquina')") or die(mysql_error());
+		
+		
+	}
+	
+	public function consultaMaquina($maquina){
+	
+			$sql = mysql_query( "SELECT * FROM MAQUINA WHERE idMaq = '$maquina'") or die(mysql_error());
+
+			return $sql;
+		
+		}
+	
+	
+	public function modificacion($idMaquina){
+				
+				mysql_query("UPDATE MAQUINA SET nomMaq = '$this->nombreMaquina'
+					WHERE idMaq = '$idMaquina'") or die(mysql_error());
+	}
+	
+	
+	public function lista(){
+	
+			$sql = mysql_query("SELECT * FROM MAQUINA");
+
+			return $sql;
+	}
+	
+	public function borrar(){
+		$borrarMaquina = "DELETE FROM MAQUINA WHERE idMaq = '$this->idMaquina'";
+		$resultado = mysql_query($borrarMaquina) or die(mysql_error());
+		return $resultado;
+	
+	}
+
+	
+}
+		
+?>
