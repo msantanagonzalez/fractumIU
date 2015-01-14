9243d2ac88101f3a5b1edf8f53810590408a9186
mlnieves <martaverin93@gmail.com>
2015-01-14 20:06:26 +0100

    función traducción

---
+++ www/View/messages/messages_es.php
@@ -0,0 +1,7 @@
+<?php
+  //file: /view/messages/messages_es.php
+  
+ // Array vacío , ninguna traducción necesaria ,
+  // Desde llaves están en español en el código fuente .
+  $i18n_messages = array()
+?>
