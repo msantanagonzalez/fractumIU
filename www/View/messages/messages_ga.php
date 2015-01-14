68d669aa50688b126956c5bf77b446a4036f63a8
mlnieves <martaverin93@gmail.com>
2015-01-14 21:10:03 +0100

    prueba multi-idioma2

--- www/View/messages/messages_ga.php
+++ www/View/messages/messages_ga.php
@@ -5,161 +5,16 @@
   
 	//View/empresas/altaEmpresa
 	
-	"EMPRESA" => "NEGOCIO"
-	"#CIF:" => "#CIF:"
-	"Nombre:" => "Nome:"
-	"Teléfono:" => "Teléfono:"
-	"Correo:" => "Correo:"
+	"EMPRESA" => "NEGOCIO",
+	"#CIF:" => "#CIF:",
+	"Nombre:" => "Nome:",
+	"Teléfono:" => "Teléfono:",
+	"Correo:" => "Correo:",
 	
 	//View/empresas/consultarJefe
-	"Correo:" => "Correo:"
-	"Correo:" => "Correo:"
-	"Correo:" => "Correo:"
-	"Correo:" => "Correo:"
-	"Correo:" => "Correo:"
-	"Correo:" => "Correo:"
-	"Correo:" => "Correo:"
-	"Correo:" => "Correo:"
-	"Correo:" => "Correo:"
-	"Correo:" => "Correo:"
-	"Correo:" => "Correo:"
-	"Correo:" => "Correo:"
-	"Correo:" => "Correo:"
-	"Correo:" => "Correo:"
-	"Correo:" => "Correo:"
+	"Correo:" => "Correo:",
 	
   
-  	//View/incidencias/altaInterno
-	//altaJefe
-	"NUEVA INCIDENCIA" => "NOVA INCIDENCIA",
-	"Apertura:" => "Apertura:",
-	"Fecha Apertura:" => "Data de Inauguracion",
-	"Maquina:" => "Maquina:",
-	"Maquina1" => "Maquina1",
-	"Maquina2" => "Maquina2",
-	"Maquina3" => "Maquina3",
-	"Descripcion:" => "Descricion:",
-  
-	//View/incidencias/consultarExterno
-	
-	"- DETALLES $IDincidencia -" => "- DETALLES $IDincidencia -",
-	"#ID Incidencia:" => "#ID Incidencia:",
-	"Nombre Maquina:" => "Nome da maquina:",
-	"Responsable:" => "Responsable:",
-	"Estado:" => "Estado:",
-	"Fecha Alta:" => "Data de Engadido:",
-	"Fecha Cierre:" => "Data de Peche:",
-	"BREVE DESCRIPCION DE APERTURA" => "BREVE DESCRICION DA APERTURA",
-	"- ITERACIONES -" => "- ITERATIONS -",
-	"ID" => "ID",
-	"FechaInicio" => "DataInicio",
-	"Coste" => "Custo",
-	
-			//esto no sé si hay que traducirlo
-	"IT001" => "IT001",
-	"15/04/2014" => "15/04/2014",
-	"12.00" => "12.00",
-	"Consultar" => "Consultar",
-	
-	"IT002" => "IT002",
-	"18/04/2014" => "18/04/2014",
-	"60.00" => "60.00",
-	
-	"IT003" => "IT003",
-	"20/04/2014" => "20/04/2014",
-	"80.00" => "80.00",
-	
-	//View/incidencias/consultarInterno
-	"Operario Alta:" => "Operador Alta:",
-	"Coste total:" => "Custo total",
-	"Estado:" => "Estado:",
-	"Operario" => "Operador",
-		//¿Esta parte es una variable se traduce o no?
-		// si no se traduce en consultarInterno cambiarlo!! puesto que está para traducir
-		// cambiar también en consultarJefe
-		
-			"$operario" => "$operador",
-	
-	
-	"0.00" => "0.00",
-	
-	//View/incidencias/consultarJefe
-	"INCIDENCIA $#IDincidencia" => "INCIDENCIA $#IDincidencia",
-	"Interno alta:" => "Interno alta:",
-	"Coste:" => "Custo:",
-	"Derivada:" => "Derivada:",
-	"Mantenimiento completo, coste de piezas no incluido." => "Mantemento integral, o custo de pezas non incluídas.",
-	"OperarioCoste" => "OperadorCusto",
-	
-	//View/incidencias/listarExterno
-	"INCIDENCIAS" => "INCIDENTES",
-	"Apertura" => "Apertura",
-	"Estado" => "Estado",
-	"Ult.Iteracion" => "Ult.Iteracion",
-	"ID001" => "ID001",
-	"Usuario1" => "Usuario1",
-	"Abierta" => "Aberto",
-	"ID003" => "ID003",
-	"Pendiente de cierre" => "Non pechado",
-	"ID004" => "ID004",
-	"En curso" => "En marcha",
-	
-	
-	//View/incidencias/listarExterno
-	"Derivar" => "Derivar",
-	"ID002" => "ID002",
-	"Usuario2" => "Usuario2",
-	"Cerrada" => "Pechada",
-	"IT004" => "IT004",
-	"Cerrar" => "Pechar",
-	
-	//View/incidencias/listarExterno
-	"#ID Inc." => "#ID Inc.",
-	"Titulo" => "Titulo",
-	"Ult. iteracion" => "Ult. iteracion",
-	"Ult. operario" => "Ult. operario",
-	"#233232" => "#233232",
-	
-	"Avería alternador" => "Avería alternador",
-	"Fulanito" => "Fulanito",
-	"12/03/2014" => "12/03/2014",
-	"Derivada" => "Derivada",
-	"#20642" => "#20642",
-	"#23342" => "#23342",
-	"Pendiente de derivar" => "Pendente de derivar",
-	
-	//View/incidencias/modificarJefe
-	
-	"#ID Maquina:" => "#ID Maquina:",
-	
-	//View/iteraciones/altaExterno
-	
-	"- TRABAJANDO $IDincidencia -" => "- TRABALLANDO $IDincidencia -",
-	"Fecha Inicio:" => "Data Iniciacion:",
-	"Fecha Fin:" => "Data Fin:",
-	"Hora Inicio:" => "Data Inicio",
-	"Hora Fin:" => "Data Fin:",
-	"Coste Trabajo:" => "Custo do Traballo:",
-	"Documentacion:" => "Documentacion:",
-	"ID Incidencia:" => "ID Incidencia:",
-	"Fecha Creacion:" => "Data de Creacion:",	
-	"Numero Trabajo:" => "Numero do Traballo:",
-	"BREVE DESCRIPCION DEL TRABAJO" => "DESCRICION BREVE DO TRABALLO",
-	
-	
-	
-	
-	//View/iteraciones/consultaInterno
-	"Operario Cierre:" => "Operador de clausura:",
-	"BREVE DESCRIPCION DEL TRABAJO REALIZADO" => "BREVE DESCRICION DO TRABALLO REALIZADO",
-	
-	
-	//View/iteraciones/consultarJefe
-	"Descripcion del trabajo realizado sobre la maquina." => "Descricion do traballo feito na maquina.",
-	
-
-//View/maquinas	
-	
 	
 	
   )
