2431a6bced15f075622bdb624e0dbde7edfda96d
mlnieves <martaverin93@gmail.com>
2015-01-16 00:04:08 +0100

    cambio 5 idioma usuarios

--- www/View/usuarios/modificarExternoJefe.php
+++ www/View/usuarios/modificarExternoJefe.php
@@ -8,26 +8,26 @@
 foreach ($datosUsuario as $usuario){
 ?>
   <script type="text/javascript" src="../../Resources/js/Validaciones.js"></script>          
-<h1 id="headerJefe"><a><i>PERFIL <?php echo $usuario['nomUsu']." ".$usuario['apellUsu']; ?></i></a></h1>
+<h1 id="headerJefe"><a><i><?= i18n("PERFIL") ?> <?php echo $usuario['nomUsu']." ".$usuario['apellUsu']; ?></i></a></h1>
 <form method="POST" onsubmit="return modificarExternoJefe()" action="../../Controller/usuariosController.php?dniUsu=<?php echo $usuario['dniUsu']; ?>">
 	<table class="default">
 		<tr> 
-			<td width="25%">#ID: </td> 
+			<td width="25%"><?= i18n("#ID:") ?> </td> 
 			<td width="25%"><input type="text" disabled class="text" name="dniUsu" value="<?php echo $usuario['dniUsu']; ?>"/></td> 
-			<td width="25%">Contraseña: </td> 
+			<td width="25%"><?= i18n("Contraseña:") ?> </td> 
 			<td width="25%"> <input type="password" class="text" name="passUsu" value="<?php echo $usuario['passUsu']; ?>"/></td> 
 		</tr>
 		<tr> 
-			<td width="25%">Nombre: </td> 
+			<td width="25%"><?= i18n("Nombre:") ?> </td> 
 			<td width="25%"><input id="nombre" type="text" class="text" name="nomUsu" value="<?php echo $usuario['nomUsu']; ?>"/></td> 
-			<td width="25%">Apellidos: </td> 
+			<td width="25%"><?= i18n("Apellidos:") ?> </td> 
 			<td width="25%"> <input id="apellidos" type="text" class="text" name="apellUsu" value="<?php echo $usuario['apellUsu']; ?>"/></td> 
 		</tr>
 		<?php
 		foreach ($datosExterno as $externo){
 		?>
 		<tr> 
-			<td width="25%">Empresa: </td> 
+			<td width="25%"><?= i18n("Empresa:") ?> </td> 
 			<td width="25%"><input id="empresa" type="text" class="text" name="cifEmpr" value="<?php echo $externo['cifEmpr']; ?>"/></td>
 			<td></td>
 			<td></td>
