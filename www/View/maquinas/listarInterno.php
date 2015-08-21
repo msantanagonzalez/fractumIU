c12f44371ebfba82d9a90824e1cdc07a8412271d
msgonzalez <msgonzalez@esei.uvigo.es>
2015-08-21 22:11:05 +0200

    Codigo maria

--- www/View/maquinas/listarInterno.php
+++ www/View/maquinas/listarInterno.php
@@ -16,19 +16,23 @@
 <div style="height:300px;width:auto;overflow-y: scroll;"><!--ESTO DA LUGAR AL SCROLL-->
 
 	<table class="default">
-		<?php 
+		<?php
+			$ultimaIncid = $_SESSION['listaIncidMaquina'];
 			$rows = $_SESSION['listaMaquina'];
+			$servicio = $_SESSION['listaServicios'];
+			$cont = 0;
 			foreach ($rows as $row) {
 		?>
-
-		<tr> 
-			<td width="20%"><?php echo $row['idMaq']; ?></td> 
-			<td width="20%"><?php echo $row['nomMaq']; ?></td> 
-			<td width="20%"><a href="#">FULL COVER</a></td> 
-			<td width="20%"><a href="../../Controller/incidenciasController.php?accion=consulta&idIncidencia">I001</a></td> 
+		<tr>
+			<td width="20%"><?php echo $row['idMaq']; ?></td>
+			<td width="20%"><?php echo $row['nomMaq']; ?></td>
+			<td width="20%"><?php echo $servicio[$cont]; ?></td>
+			<td width="20%"><a href="../../Controller/incidenciasController.php?accion=Consulta&idIncid=<?php echo $ultimaIncid[$cont][0][0];?>"><?php echo $ultimaIncid[$cont][0][0]; ?></td>
 			<td width="20%"><a href="../../Controller/maquinasController.php?accion=Consulta&idMaq=<?php echo $row['idMaq'];?>"><button >Consultar</button></a></td>
 		</tr>
-		<?php } ?>
+		<?php
+		 $cont++;
+		} ?>
 	</table>
 </div>
 
