7606285286b98b3617a35887bc0977d6bc054fdd
msgonzalez <msgonzalez@esei.uvigo.es>
2015-09-05 22:09:26 +0200

    Asignacion de trabajo para la iteracion 4

--- www/View/usuarios/consultarExternoJefe.php
+++ www/View/usuarios/consultarExternoJefe.php
@@ -1,7 +1,7 @@
 <?php
 	$userType="jefe";
 	require_once $_SESSION['cribPath'].'View/structure/header.php';
-	
+
 	$datosUsuario = $_SESSION["datosUsuario"];
 	$datosExterno = $_SESSION["datosExterno"];
 	foreach ($datosUsuario as $usuario){
@@ -9,23 +9,23 @@
 <h1 id="headerJefe"><a><i><?= i18n("PERFIL") ?> <?php echo $usuario['nomUsu']." ".$usuario['apellUsu']; ?></i></a></h1>
 <form method="POST" action="modificarExternoJefe.php">
 	<table class="default">
-		<tr> 
-			<td width="25%"><?= i18n("#ID:") ?> </td> 
+		<tr>
+			<td width="25%"><?= i18n("#ID:") ?> </td>
 			<td width="25%"><input type="text" class="text" name="peID" disabled value="<?php echo $usuario['dniUsu']; ?>"/></td>
-			<td width="25%"><?= i18n("Contraseña:") ?> </td> 
+			<td width="25%"><?= i18n("Contraseña:") ?> </td>
 			<td width="25%"> <input type="password" class="text" name="pePass" disabled value="<?php echo $usuario['passUsu']; ?>"/></td>
 		</tr>
-		<tr> 
-			<td width="25%"><?= i18n("Contraseña:") ?> </td> 
+		<tr>
+			<td width="25%"><?= i18n("Nombre:") ?> </td>
 			<td width="25%"><input type="text" class="text" name="peNombre" disabled value="<?php echo $usuario['nomUsu']; ?>"/></td>
-			<td width="25%"><?= i18n("Apellidos:") ?> </td> 
-			<td width="25%"> <input type="text" class="text" name="peApellidos" disabled value="<?php echo $usuario['apellUsu']; ?>"/></td> 
+			<td width="25%"><?= i18n("Apellidos:") ?> </td>
+			<td width="25%"> <input type="text" class="text" name="peApellidos" disabled value="<?php echo $usuario['apellUsu']; ?>"/></td>
 		</tr>
 		<?php
 		foreach ($datosExterno as $externo){
 		?>
-		<tr> 
-			<td width="25%"><?= i18n("Empresa:") ?> </td> 
+		<tr>
+			<td width="25%"><?= i18n("Empresa:") ?> </td>
 			<td width="25%"><input type="text" class="text" name="peEmpresa" disabled value="<?php echo $externo['cifEmpr']; ?>"/></td>
 			<td></td>
 			<td></td>
@@ -33,7 +33,7 @@
 		<?php } ?>
 		<tr>
 			<td width="20%" colspan="4"><input type="submit" name="peModificar" value="Modificar"></td>
-		</tr> 
+		</tr>
 	</table>
 </form>
 
--- www/View/usuarios/consultarInternoInterno.php
+++ www/View/usuarios/consultarInternoInterno.php
@@ -1,10 +1,10 @@
 <?php
 	$userType="interno";
 	require_once $_SESSION['cribPath'].'View/structure/header.php';
-	
+
 	$datosUsuario = $_SESSION["datosUsuario"];
 	$datosInterno = $_SESSION["datosInterno"];
-	
+
 foreach ($datosUsuario as $usuario){
 ?>
 
@@ -12,31 +12,31 @@ foreach ($datosUsuario as $usuario){
 <form name='FormPerfil' id='FormPerfil' method='post' onsubmit='' action="modificarInternoInterno.php">
 	<div style='height:350px;width:auto;overflow-y: scroll;'>
 		<table class="default">
-		<tr> 
-			<td width="25%"><?= i18n("Contraseña:") ?> </td> 
-			<td width="25%"><input type="text" class="text" disabled name="piNombre" value="<?php echo $usuario['nomUsu']; ?>"/></td> 
-			<td width="25%"><?= i18n("Apellidos:") ?> </td> 
-			<td width="25%"> <input type="text" class="text" disabled name="piApellidos" value="<?php echo $usuario['apellUsu']; ?>"/></td> 
+		<tr>
+			<td width="25%"><?= i18n("Nombre:") ?> </td> 
+			<td width="25%"><input type="text" class="text" disabled name="piNombre" value="<?php echo $usuario['nomUsu']; ?>"/></td>
+			<td width="25%"><?= i18n("Apellidos:") ?> </td>
+			<td width="25%"> <input type="text" class="text" disabled name="piApellidos" value="<?php echo $usuario['apellUsu']; ?>"/></td>
 		</tr>
-		<tr> 
-			<td width="25%"><?= i18n("#ID:") ?> </td> 
-			<td width="25%"><input type="text" class="text" disabled name="piID" value="<?php echo $usuario['dniUsu']; ?>"/></td> 
-			<td width="25%"><?= i18n("Contraseña:") ?> </td> 
-			<td width="25%"> <input type="password" class="text" disabled name="piPass" value="<?php echo $usuario['passUsu']; ?>"/></td> 
+		<tr>
+			<td width="25%"><?= i18n("#ID:") ?> </td>
+			<td width="25%"><input type="text" class="text" disabled name="piID" value="<?php echo $usuario['dniUsu']; ?>"/></td>
+			<td width="25%"><?= i18n("Contraseña:") ?> </td>
+			<td width="25%"> <input type="password" class="text" disabled name="piPass" value="<?php echo $usuario['passUsu']; ?>"/></td>
 		</tr>
 		<?php
 		foreach ($datosInterno as $interno){
 		?>
-		<tr> 
-			<td width="25%"><?= i18n("Teléfono:") ?> </td> 
-			<td width="25%"><input type="text" class="text" disabled name="piTelf" value="<?php echo $interno['telefOpeInt']; ?>"/></td> 
-			<td width="25%"><?= i18n("Correo:") ?> </td> 
-			<td width="25%"> <input type="text" class="text" disabled name="piCorreo" value="<?php echo $interno['mailOpeInt']; ?>"/></td> 
+		<tr>
+			<td width="25%"><?= i18n("Teléfono:") ?> </td>
+			<td width="25%"><input type="text" class="text" disabled name="piTelf" value="<?php echo $interno['telefOpeInt']; ?>"/></td>
+			<td width="25%"><?= i18n("Correo:") ?> </td>
+			<td width="25%"> <input type="text" class="text" disabled name="piCorreo" value="<?php echo $interno['mailOpeInt']; ?>"/></td>
 		</tr>
 		<?php } ?>
 		<tr>
 			<td width="20%" colspan="4"><input type="submit" name="piModificar" value="Modificar"></td>
-		</tr> 
+		</tr>
 	</table>
 	</div>
 </form>
